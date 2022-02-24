c18032c25458a78419bedd263dd831d2d8faf0e8
kareemAmarah95 <kareemamarah2356@gmail.com>
2022-02-24 23:03:55 +0200

    m

--- src/App.js
+++ src/App.js
@@ -51,7 +51,7 @@ function App() {
   return (
     <div className="App">
       <header>
-        <h1>Kareem Todo List</h1>
+        <h1>Todo List</h1>
       </header>
       <Form
         inputText={inputText}
